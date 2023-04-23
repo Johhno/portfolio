03073af2c690f4f110ab2df5a85088b4dbd3cbf2
John Tchen <johntchen.mos@gmail.com>
2023-04-23 16:48:24 +0200

    création Purchase

--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Purchase;
 
 use App\Cart\CartService;
 use App\Entity\Purchase;
+use App\Entity\PurchaseItem;
 use App\Form\CartConfirmationType;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Routing\RouterInterface;
@@ -20,13 +23,15 @@ class PurchaseConfirmationController
     protected $router;
     protected $security;
     protected $cartService;
+    protected $em;
 
-    public function __construct(FormFactoryInterface $formFactory, RouterInterface $router, Security $security, CartService $cartService)
+    public function __construct(FormFactoryInterface $formFactory, RouterInterface $router, Security $security, CartService $cartService, EntityManagerInterface $em)
     {
         $this->formFactory = $formFactory;
         $this->router = $router;
         $this->security = $security;
         $this->cartService = $cartService;
+        $this->em = $em;
     }
 
     /**
@@ -59,16 +64,38 @@ class PurchaseConfirmationController
             return new RedirectResponse($this->router->generate('cart_show'));
         }
 
-        $purchase = $form->getData();
+
 
 
         // 5. Nous allons créer une purchase
-
+        /** @var Purchase */
+        $purchase = $form->getData();
 
         // 6. Nous allons la lier avec l'utilisateur actuellement connecté (Security)
+        $purchase->setUser($user)
+            ->setPurchasedAt(new DateTime());
+
 
         // 7. Nous allons la lier avec les produits qui sont dans le panier (CartService)
+        foreach ($this->cartService->getDetailedCartItems() as $cartItem) {
+            $purchaseItem = new PurchaseItem;
+            $purchaseItem->setPurchase($purchase)
+                ->setProduct($cartItem->product)
+                ->setProductName($cartItem->product->getName())
+                ->setProductPrice($cartItem->product->getPrice())
+                ->setQuantity($cartItem->qty)
+                ->setTotal($cartItem->getTotal());
+
+
+            $this->em->persist($purchaseItem);
+        }
 
         // 8. Nous allons enregister la commande (EntityManagerInterface)
+        $purchase->setTotal($this->cartService->getTotal());
+        $this->em->persist($purchase);
+        $this->em->flush();
+
+        $flashBag->add('success', 'La commande a bien été enregistré');
+        return new RedirectResponse($this->router->generate('purchase_index'));
     }
 }
