23da9969bbe792cc75a42c1cf3d0e64e5d3e0431
John Tchen <johntchen.mos@gmail.com>
2023-04-24 10:48:28 +0200

    Créer une Classe qui enregistre une Purchase

--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -7,6 +7,7 @@ use App\Entity\Purchase;
 use App\Cart\CartService;
 use App\Entity\PurchaseItem;
 use App\Form\CartConfirmationType;
+use App\Purchase\PurchasePersister;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,11 +22,13 @@ class PurchaseConfirmationController extends AbstractController
 
     protected $cartService;
     protected $em;
+    protected $persister;
 
-    public function __construct(CartService $cartService, EntityManagerInterface $em)
+    public function __construct(CartService $cartService, EntityManagerInterface $em, PurchasePersister $persister)
     {
         $this->cartService = $cartService;
         $this->em = $em;
+        $this->persister = $persister;
     }
 
     /**
@@ -62,28 +65,7 @@ class PurchaseConfirmationController extends AbstractController
         /** @var Purchase */
         $purchase = $form->getData();
 
-        // 6. Nous allons la lier avec l'utilisateur actuellement connecté (Security)
-        $purchase->setUser($user)
-            ->setPurchasedAt(new DateTime());
-
-        // 7. Nous allons la lier avec les produits qui sont dans le panier (CartService)
-        foreach ($this->cartService->getDetailedCartItems() as $cartItem) {
-            $purchaseItem = new PurchaseItem;
-            $purchaseItem->setPurchase($purchase)
-                ->setProduct($cartItem->product)
-                ->setProductName($cartItem->product->getName())
-                ->setProductPrice($cartItem->product->getPrice())
-                ->setQuantity($cartItem->qty)
-                ->setTotal($cartItem->getTotal());
-
-            $this->em->persist($purchaseItem);
-        }
-
-        // 8. Nous allons enregister la commande (EntityManagerInterface)
-        $purchase->setTotal($this->cartService->getTotal());
-        $this->em->persist($purchase);
-
-        $this->em->flush();
+        $this->persister->storePurchase($purchase);
 
         $this->cartService->empty();
 
