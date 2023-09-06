b3de3110f85058f17cb6a7cf517733b91732ec87
John Tchen <johntchen.mos@gmail.com>
2023-09-06 11:27:25 +0200

    Env Stripe Key

---
+++ src/Service/StripeService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Product;
+
+class StripeService
+{
+
+    private $privateKey;
+
+    public function __construct()
+    {
+        if ($_ENV['APP_ENV'] === 'dev') {
+            $this->privateKey = $_ENV['STRIPE_SECRET_KEY_TEST'];
+        } else {
+            $this->privateKey = $_ENV['STRIPE_SECRET_KEY_LIVE'];
+        }
+    }
+}
